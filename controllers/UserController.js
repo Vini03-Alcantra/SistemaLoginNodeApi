e0e780db23900f073afb807a9999ede415e79c4d
Vinicus <viniciusalcantarabarros@gmail.com>
2021-04-16 14:44:51 -0300

    realizando validações no create do UserController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,8 +2,26 @@ class UserController{
     async index(req, res){}
 
     async create(req, res){
-        console.log(req.body)
-        res.send("Pegando  ocorpo da requisição")
+        var {email, name, password} = req.body;
+        
+        if (email == undefined) {
+            res.statusCode = 400;
+            res.json({err: "This email is invalid"})
+        }
+        
+        if (name == undefined) {
+            res.statusCode = 400;
+            res.json({err: "This name is invalid"})
+        }
+
+        if (password == undefined) {
+            res.statusCode = 400;
+            res.json({err: "This email is empty"})
+        }
+
+
+        res.statusCode = 200;
+        res.send("Tudo ok")
     }
 }
 
