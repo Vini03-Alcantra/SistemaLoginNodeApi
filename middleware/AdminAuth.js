85bca0dbd24250f5f0b71eaa12fd19e04d0ff291
Vinicus <viniciusalcantarabarros@gmail.com>
2021-04-19 00:01:57 -0300

    Finalizando projeto com midleware

--- middleware/AdminAuth.js
+++ middleware/AdminAuth.js
@@ -1,19 +1,32 @@
 var jwt = require("jsonwebtoken")
 var secret = "jierji034i0rt09549854jgbji0t4ji0tgjefu9hrfi0j?;v,rpkl";
 
-module.exports = function(req, res){
+module.exports = function(req, res, next){
     const authToken = req.headers['authorization'];
 
     if (authToken != undefined) {
         const bearer = authToken.split(' ');
         var token = bearer[1]
 
-        var decoded = jwt.verify(token, secret);
-        console.log(decoded)
-        next()
+        try {            
+            var decoded = jwt.verify(token, secret);
+            if (decoded.role) {
+                next()                
+            }else{
+                res.status(403)
+                res.send("Você não tem permissão")
+                return;
+            }
+        } catch (error) {
+            res.status(403)
+            res.send("Você não está codificado")
+            return
+        }
+
     } else {
         res.status(403)
         res.send("Você não está autenticado")
         return
     }
 }
+
